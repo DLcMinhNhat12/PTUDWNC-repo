15ec42f2993f353d31a586fc4cd63502387970f7
Minh Nhat12 <tdmnhatt@gmail.com>
2023-04-09 00:33:44 +0700

    React vs TatBlog

--- src/TipsAndTricks/TatBlog.Core/DTO/PagingParams.cs
+++
@@ -1,9 +0,0 @@
-﻿using TatBlog.Core.Contracts;
-
-public class PagingParams : IPagingParams
-{
-	public int PageSize { get; set; } = 10;
-	public int PageNumber { get; set; } = 1;
-	public string SortColumn { get; set; } = "Id";
-	public string SortOrder { get; set; } = "ASC";
-}
